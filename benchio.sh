4a9882c44e22a461300e3e24d4cf979c39e35dc0
Adrian Jackson <adrianj@epcc.ed.ac.uk>
2020-09-28 12:54:37 +0100

    updating batch file for Cirrus

--- benchio.sh
+++ benchio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash --login
 
-#SBATCH --nodes=4
+#SBATCH --nodes=2
 #SBATCH --time=0:10:0
 #SBATCH --partition=standard
 #SBATCH --qos=standard
@@ -9,13 +9,18 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --exclusive
 
+module load intel-compilers-18
+module load hdf5parallel/1.10.6-intel18-mpt222
+module load netcdf-parallel/4.7.4-intel18-mpt222
+
+export MPI_TYPE_DEPTH=20
 
 echo "Starting job $SLURM_JOB_ID at `date`"
 
-for p in 1 36 72 144
+for p in 1 36 72
 do
 
-srun -n $p ./benchio
+mpiexec_mpt -ppn 36 -n $p ./benchio
 
 done
 
